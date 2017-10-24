dc2c9b2901ed28fb75f56c7788f3d853528bf161
Ruiter Matos <ruiter@fretebras.com.br>
2017-10-24 10:39:56 -0200

    Initial commit

---
+++ shared/src/main/java/App.kt
@@ -0,0 +1,11 @@
+import android.app.Application
+
+/**
+ * Created by ruitermatos on 24/10/17.
+ */
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ shared/src/main/java/BaseActivity.kt
@@ -0,0 +1,7 @@
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by ruitermatos on 24/10/17.
+ */
+class BaseActivity : AppCompatActivity() {
+}
