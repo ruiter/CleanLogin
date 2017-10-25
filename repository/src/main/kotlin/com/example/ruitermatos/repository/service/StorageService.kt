53fb46efa4c76c9736c383f2bd8b8349715b2b7f
Ruiter Matos <ruiter@fretebras.com.br>
2017-10-25 11:52:30 -0200

    criando pacote repository onde vao ficar arquivos compartilhados

---
+++ repository/src/main/kotlin/com/example/ruitermatos/repository/service/StorageService.kt
@@ -0,0 +1,7 @@
+package com.example.ruitermatos.repository.service
+
+/**
+ * Created by ruitermatos on 25/10/17.
+ */
+class StorageService {
+}
---
+++ repository/src/main/kotlin/com/example/ruitermatos/repository/storage/StorageRepository.kt
@@ -0,0 +1,7 @@
+package com.example.ruitermatos.repository.storage
+
+/**
+ * Created by ruitermatos on 25/10/17.
+ */
+interface StorageRepository {
+}
